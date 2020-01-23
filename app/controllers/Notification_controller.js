b523bff648bd1115f90b336926467605b45e625d
Md. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-01-23 21:39:55 +0600

    getReceivedNotifications and getSentNotifications data prepared backend

--- app/controllers/Notification_controller.js
+++ app/controllers/Notification_controller.js
@@ -5,7 +5,7 @@ exports.notificationSubmit=function (req, res, next) {
     console.log("===============>");
     const mybodydata = {
         sender_id:user._id,
-        receiver_id:req.body._id,
+        receiver_id:req.body.user_id,
         status:"Pending",
         message:req.body.message
     }
@@ -16,3 +16,20 @@ exports.notificationSubmit=function (req, res, next) {
     res.send("Notification sent");
     
   }
+
+exports.getReceivedUserNotification = function (req, res, next) {
+    var user=req.session.user;
+    NotificationSchema.find({receiver_id:user._id},function (err,result) {
+        if(err) throw err;
+        res.send(result);
+    })
+
+}
+
+exports.getSentUserNotification = function (req, res, next) {
+    var user=req.session.user;
+    NotificationSchema.find({sender_id:user._id},function (err,result) {
+        if(err) throw err;
+        res.send(result);
+    })
+}
--- routes/notification.js
+++ routes/notification.js
@@ -5,5 +5,8 @@ var home = require('../app/controllers/home');
 var NotificationController = require('../app/controllers/Notification_controller');
 router.post('/createNotification',home.loggedIn,NotificationController.notificationSubmit);
 
+router.get('/getReceivedNotifications',home.loggedIn,NotificationController.getReceivedUserNotification);
+router.get('/getSentNotifications',home.loggedIn,NotificationController.getSentUserNotification);
+
 
 exports = module.exports =router;
