cab500c2a7d3244de3e23095bfd55aebcef6900a
Md. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-01-23 21:05:40 +0600

    notification saved in back end

--- app/controllers/Notification_controller.js
+++ app/controllers/Notification_controller.js
@@ -1,5 +1,18 @@
+var NotificationSchema=require('../models/notifications');
 
-exports.notificationSubmit = function(req, res, next){
-    console.log(req.body);
-	res.send('ok');
-};
+exports.notificationSubmit=function (req, res, next) {
+    var user=req.session.user;
+    console.log("===============>");
+    const mybodydata = {
+        sender_id:user._id,
+        receiver_id:req.body._id,
+        status:"Pending",
+        message:req.body.message
+    }
+    var notification = NotificationSchema(mybodydata); 
+    notification.save(function(err,products){
+        if(err) res.send("Notification not sent");
+      });
+    res.send("Notification sent");
+    
+  }
--- app/controllers/index_controller.js
+++ app/controllers/index_controller.js
@@ -39,3 +39,66 @@ exports.finddonor = function(req, res, next){
 exports.profile = function(req, res, next){
 	res.render("profile");
 };
+exports.editProfile=function (req, res, next) {
+    console.log(req.body);
+    var user=req.session.user;
+    console.log("===============>");
+    console.log(user);
+
+
+    // profileSchema.find({
+    //   user_id:user._id;
+    // },
+    // function(err, results) {
+    //   if (err) throw err;
+    //   if (results.toString() === '') {
+
+
+    //     res.redirect('/profile');
+    //   }
+    //   console.log(results);
+    //   res.render('home', {
+        
+    //   });
+    // });
+
+    var query = {
+    'user_id': user._id
+  };
+  
+    const mybodydata = {
+            user_id:user._id,
+            fullname: req.body.fullname,
+            address: req.body.address,
+            district: req.body.district,
+            country: req.body.country,
+            bloodgroup: req.body.bloodgroup,
+            lastgiven: req.body.lastgiven,
+            medicalissue: req.body.medicalissue
+
+    }
+    var data = profileSchema(mybodydata); 
+    //var data = UsersModel(req.body);
+    profileSchema.findOneAndUpdate(query,{
+      $set:{
+            user_id:user._id,
+            fullname: req.body.fullname,
+            address: req.body.address,
+            district: req.body.district,
+            country: req.body.country,
+            bloodgroup: req.body.bloodgroup,
+            lastgiven: req.body.lastgiven,
+            medicalissue: req.body.medicalissue
+      }
+    },{
+      new: true,
+      upsert: true
+
+    },function (err,doc) {
+      if (err) {
+       res.render('profile',{message: 'User registered not successfully!'});
+      } else {
+       res.render('profile',{message: 'User registered successfully!'});
+      }
+    })
+  }
